657dc4adc3fd1fbef863f75d242b698b805ef86f
ajugoon <54783263+ajugoon@users.noreply.github.com>
2019-12-16 23:21:52 -0500

    Update and rename loop-ex2.py to python-loops/loops4.py

--- python-loops/loops4.py
+++ python-loops/loops4.py
@@ -1,4 +1,4 @@
-# Loop Example 2
+# Loop Example 4
 # Here we use a structure known as a list which is contained in square
 # brackets, where 'x' will be assigned one value at a time as the list
 # is examined
